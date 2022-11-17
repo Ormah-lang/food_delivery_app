6d108ed7944a2fb2238f7680a8703ba168577bde
Chioma Onyeneke <ccchioma60@gmail.com>
2022-11-17 17:46:40 +0000

    implement scaling property on the food page skeleton

--- lib/home_page/food_page_body.dart
+++ lib/home_page/food_page_body.dart
@@ -13,10 +13,31 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
-  PageController pageController = PageController(viewportFraction: 0.9);
+  PageController pageController = PageController(viewportFraction: 0.85);
+  //initial value of the current page
+  var _currPageValue = 0.0;
+  //initial value of the scale factor
+  double _scaleFactor = 0.8;
+  double _height = 220;
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        //assigns the current page value to _currPageValue and should be non-null
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       // color: Colors.red,
       height: 320,
       child: PageView.builder(
@@ -29,97 +50,121 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(children: [
-      Container(
-        height: 220,
-        margin: const EdgeInsets.only(left: 10, right: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: Colors.white,
-          // index.isEven ? const Color(0xFFCD5700) : const Color(0xFF9294cc),
-          image: const DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/image/jollof_rice.png'),
-          ),
-        ),
-      ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: 120,
-          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 30),
+    //for scaling the foodPage along the y axis,
+    //create an instance of Matrix4
+    Matrix4 matrix = Matrix4.identity();
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
-            color: Colors.white,
+            color: index.isEven
+                ? const Color(0xFFCD5700)
+                : const Color(0xFF9294cc),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/image/jollof_rice.png'),
+            ),
           ),
-          child: Container(
-              padding: const EdgeInsets.only(
-                top: 10,
-                left: 15,
-                right: 15,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(
-                    text: 'Jollof Rice',
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Wrap(
-                        spacing: -2,
-                        children: List.generate(
-                            5,
-                            (index) => Icon(
-                                  Icons.star,
-                                  color: AppColors.mainColor,
-                                  size: 16.0,
-                                )),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      SmallText(text: '4.5'),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      SmallText(
-                        text: '1287',
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      SmallText(
-                        text: 'comments',
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconAndText(
-                          icon: Icons.circle_sharp,
-                          text: 'Normal',
-                          iconColor: AppColors.iconColor1),
-                      IconAndText(
-                          icon: Icons.location_on,
-                          text: '1.7km',
-                          iconColor: AppColors.mainColor),
-                      IconAndText(
-                          icon: Icons.access_time,
-                          text: '32min',
-                          iconColor: AppColors.iconColor2),
-                    ],
-                  )
-                ],
-              )),
         ),
-      )
-    ]);
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 20, right: 20, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+                padding: const EdgeInsets.only(
+                  top: 10,
+                  left: 15,
+                  right: 15,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(
+                      text: 'Jollof Rice',
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Wrap(
+                          spacing: -2,
+                          children: List.generate(
+                              5,
+                              (index) => Icon(
+                                    Icons.star,
+                                    color: AppColors.mainColor,
+                                    size: 16.0,
+                                  )),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        SmallText(text: '4.5'),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        SmallText(
+                          text: '1287',
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        SmallText(
+                          text: 'comments',
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndText(
+                            icon: Icons.circle_sharp,
+                            text: 'Normal',
+                            iconColor: AppColors.iconColor1),
+                        IconAndText(
+                            icon: Icons.location_on,
+                            text: '1.7km',
+                            iconColor: AppColors.mainColor),
+                        IconAndText(
+                            icon: Icons.access_time,
+                            text: '32min',
+                            iconColor: AppColors.iconColor2),
+                      ],
+                    )
+                  ],
+                )),
+          ),
+        )
+      ]),
+    );
   }
 }
