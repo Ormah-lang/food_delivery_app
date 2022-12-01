6a30d84439442b4085e9b1f0d843b95fca9a3122
Chioma Onyeneke <ccchioma60@gmail.com>
2022-12-01 15:41:26 +0000

    add classes that allows user to get data from server

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_app/screens/food/recommended_food_detail.dart';
 import 'package:get/get.dart';
 
-void main() {
+import 'helper/dependencies.dart' as dep;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
